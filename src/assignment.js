31017b5eb6ad0be250183887f2bafe01ecc04547
Sarah Ukoro <79695423+sarah-u@users.noreply.github.com>
2022-07-15 20:11:03 +0000

    Attempted challenge 1 and 2.

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,9 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
